b7f2fcff8fbe643b97bb34b3a42d51051eae086c
Bejaoui Majed <majedbejaouipro@gmail.com>
2021-07-29 13:06:37 +0200

    middelware

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-sm-4">
+<form action="login" method="POST">
+  <div class="mb-3">
+      @csrf
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+
+</div>
+
+</div>
+
+</div>
+@endsection
